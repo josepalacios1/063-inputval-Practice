7d2de8bcf0812c93efb86324cf8438a11c006951
josepalacios1 <43544155+josepalacios1@users.noreply.github.com>
2019-03-20 10:07:47 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+//create a click handler for the Post button.
+$("button").click(function
+
+
+
+
+
+
+
+// header toggles brands when clicked :)
+(var header = $(".header");
+header.click(function () ;
+    header.toggleClass("instagram");
+    var brand = header.text();
+     header.text(brand === "Instagram" ? "Facebook" : "Instagram");
+});
